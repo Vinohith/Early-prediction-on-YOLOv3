bb7d63aa6ba2b5eed66853084c0f393175b4ea86
P Vinohith Reddy <vinohith1998@gmail.com>
2018-06-30 11:48:51 +0530

    Update yolo_model.py

--- model/yolo_model.py
+++ model/yolo_model.py
@@ -227,7 +227,7 @@ class YOLO:
             print(scores.shape)
             acc = np.array(scores)
             print(acc.shape)
-            if acc.all()>1:
+            if acc.all()>0.9:
                 print('Early prediction at layer {}'.format(layer[i-1]))
                 
         
